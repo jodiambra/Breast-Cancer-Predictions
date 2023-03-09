294dfcccdfa4f0e9d456b13ffbb0c197be6d4ba4
Jacques Diambra Odi <115895428+jodiambra@users.noreply.github.com>
2023-03-09 10:10:02 -0500

    predict proba

--- prediction.py
+++ prediction.py
@@ -2,3 +2,7 @@ import joblib
 def predict(data):
     model = joblib.load('Model/final_model.sav')
     return model.predict(data)
+
+def predict_proba(data):
+    model = joblib.load('Model/final_model.sav')
+    return model.predict_proba(data)
